d8cc507bb006817fb80b73e44c67dd2ba412bca8
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2021-12-11 12:30:30 +0100

    Improve Day 3 OCaml

--- ocaml/day03.ml
+++ ocaml/day03.ml
@@ -1,7 +1,5 @@
 open Core
 
-let ceil_half n = 
-  int_of_float @@ Float.round_up @@ (float_of_int n) /. 2.  
   
 let int_of_bit = function
   | '1' -> 1
@@ -30,9 +28,8 @@ let part1 =
     match binaries with
     | [] -> counter
     | binary :: others
-      -> count_bits
-           others
-           (List.mapi ~f:(inc_to_counter binary) counter) in
+      -> count_bits others
+           @@ List.mapi ~f:(inc_to_counter binary) counter in
 
   let most_commons = List.map ~f:(fun x -> if x > threshold then "1" else "0") (count_bits bins counter) in
   let least_commons = List.map ~f:(function "1" -> "0" | _ -> "1") most_commons in
@@ -41,39 +38,39 @@ let part1 =
   |> string_of_int
 
 
-  let part2 =
-    let inc_to_counter char count =
-      count + (int_of_bit char) in
-    let rec count_ones binaries index count = 
-      match binaries with
-      | [] -> count
-      | binary :: others
-        -> count_ones others
-             index
-             (inc_to_counter (String.get binary index) count) in
-    
-    let rec get_last predicate binaries index = match binaries with
+let part2 =
+  let rec get_last predicate binaries index =
+    let count_ones binaries index =
+      let to_int bin =
+        String.get bin index |> int_of_bit in
+      List.map ~f:to_int binaries |> List.fold ~f:(+) ~init:0 in
+
+    let ceil_half n = 
+      int_of_float @@ Float.round_up @@ (float_of_int n) /. 2. in
+
+    match binaries with
     | [result] -> result
     | more -> begin 
         let threshold = ceil_half (List.length binaries)
-        and count = count_ones binaries index 0 in
+        and count = count_ones binaries index in
         get_last 
           predicate 
           (List.filter ~f:(predicate threshold count index) more)
           (index + 1)
     end in
 
-    let filter_most_common threshold count index binary =
-      let char = String.get binary index in
-      Char.equal char @@ if count >= threshold then '1' else '0' in
-    let last_with_most_commons = get_last filter_most_common bins 0
-    and filter_least_common threshold count index binary =
-      let char = String.get binary index in
-      Char.equal char @@ if count >= threshold then '0' else '1' in
-    let last_with_least_commons =  get_last filter_least_common bins 0 in
+  let filter_most_common threshold count index binary =
+    let char = String.get binary index in
+    Char.equal char @@ if count >= threshold then '1' else '0'
+  and filter_least_common threshold count index binary =
+    let char = String.get binary index in
+    Char.equal char @@ if count >= threshold then '0' else '1' in
   
-    (last_with_most_commons |> int_of_bin) * (last_with_least_commons |> int_of_bin)
-    |> string_of_int
+  let last_with_most_commons = get_last filter_most_common bins 0
+  and last_with_least_commons =  get_last filter_least_common bins 0 in
+
+  (last_with_most_commons |> int_of_bin) * (last_with_least_commons |> int_of_bin)
+  |> string_of_int
 
 let _ =
   print_endline part1;
