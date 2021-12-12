64a1f28bdc83004421299962a86147e93421f4c7
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2021-12-12 17:55:58 +0100

    Close #1: Use deque for day 6 Python

--- python/day06.py
+++ python/day06.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from collections import defaultdict
+from collections import deque
 
 
 def get_fish():
@@ -8,17 +8,13 @@ def get_fish():
 
 
 def population_after(generations):
-    current_fish = Counter(get_fish())
+    fish_count = Counter(get_fish())
+    population = deque([fish_count[k] for k in range(9)])
     for _ in range(generations):
-        new_fish = defaultdict(int)
-        for days_remaining, count in current_fish.items():
-            if days_remaining == 0:
-                new_fish[8] += count
-                new_fish[6] += count
-            else:
-                new_fish[days_remaining - 1] += count
-        current_fish = new_fish
-    return Counter(current_fish).total()
+        zeros = population.popleft()
+        population.append(zeros)
+        population[6] += zeros
+    return sum(population)
 
 
 if __name__ == "__main__":
